0512292fe463bd685a9af429446a2bcfc31e72bc
Ari Seyhun <ariseyhun@live.com.au>
2022-12-08 00:52:47 +0800

    docs: add example to crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -8,6 +8,28 @@
 //!
 //! For more information, see the Message DB project page.
 //! <https://github.com/message-db/message-db>
+//!
+//! # Example
+//!
+//! ```ignore
+//! use message_db::database::{MessageDb, WriteMessageOpts};
+//! use serde_json::json;
+//!
+//! // Connect to MessageDb
+//! let message_db = MessageDb::connect("postgres://postgres:password@localhost:5432/postgres").await?;
+//!
+//! // Get last stream message
+//! let last_message = MessageDb::get_last_stream_message(&message_db, "account-123", None).await?;
+//!
+//! // Write message
+//! let last_message = MessageDb::write_message(
+//!     &message_db,
+//!     "account-123",
+//!     "AccountOpened",
+//!     &json!({ "initial_balance": 0 }),
+//!     &WriteMessageOpts::default(),
+//! ).await?;
+//! ```
 
 #![warn(missing_docs)]
 #![cfg_attr(docsrs, feature(doc_cfg, doc_auto_cfg))]
